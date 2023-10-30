f3a9bd17bf83469159de4161fadba1f5e4b6a5e8
Yee Y <s0m3y3@gmail.com>
2023-10-30 18:15:13 -0500

    updated some todo items.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    new WebpackPwaManifest({
+      //missing info ``````````
+    }),
+    new InjectManifest({
+            //missing info ``````````
+    }),
     ],
 
     module: {
       rules: [
-        
-      ],
+        {
+          test: /\.css$/i,
+          use: ['css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+        }
     },
   };
 };
